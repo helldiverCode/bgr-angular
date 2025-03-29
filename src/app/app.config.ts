cc78d4686bf657364a3283ee2594e5900e071ea4
helldiverCode <37001689+helldiverCode@users.noreply.github.com>
2025-03-29 16:21:18 +0100

    fix interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,15 @@
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { httpErrorInterceptor } from './shared/interceptor/error-handler.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi()),
+    provideHttpClient(withInterceptors([httpErrorInterceptor])),
     provideNoopAnimations(),
   ],
 };
--- src/app/shared/interceptor/error-handler.interceptor.ts
+++ src/app/shared/interceptor/error-handler.interceptor.ts
@@ -1,62 +1,63 @@
-import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandlerFn,
+  HttpRequest
+} from '@angular/common/http';
+import { inject } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { inject } from '@angular/core';
-import { Router } from '@angular/router';
-import { MatSnackBar } from '@angular/material/snack-bar';
 
-@Injectable({
-  providedIn: 'root' // No need to provide it manually in a module
-})
-export class HttpErrorInterceptor implements HttpInterceptor {
-  private router = inject(Router);
-  private snackBar = inject(MatSnackBar);
+export function httpErrorInterceptor(
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn
+): Observable<HttpEvent<unknown>> {
+  const router = inject(Router);
+  const snackBar = inject(MatSnackBar);
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      let errorMessage = 'An unexpected error occurred';
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(req).pipe(
-      catchError((error: HttpErrorResponse) => {
-        let errorMessage = 'An unexpected error occurred';
-
-        if (error.error instanceof ErrorEvent) {
-          // Client-side error
-          errorMessage = `Client Error: ${error.error.message}`;
-        } else {
-          // Server-side error
-          switch (error.status) {
-            case 400:
-              errorMessage = 'Bad request. Please check your input.';
-              if (error.message) {
-                errorMessage += error.message;
-              }
-              break;
-            case 401:
-              errorMessage = 'Unauthorized. Redirecting to login...';
-              this.router.navigate(['/login']);
-              break;
-            case 403:
-              errorMessage = 'Access forbidden. Contact the administrator.';
-              break;
-            case 404:
-              errorMessage = 'Requested resource not found.';
-              break;
-            case 500:
-              errorMessage = 'Internal Server Error. Try again later.';
-              break;
-            default:
-              errorMessage = `Error ${error.status}: ${error.message}`;
-          }
+      if (error.error instanceof ErrorEvent) {
+        // Client-side error
+        errorMessage = `Client Error: ${error.error.message}`;
+      } else {
+        // Server-side error
+        switch (error.status) {
+          case 400:
+            errorMessage = 'Bad request. Please check your input.';
+            if (error.message) {
+              errorMessage += error.message;
+            }
+            break;
+          case 401:
+            errorMessage = 'Unauthorized. Redirecting to login...';
+            router.navigate(['/login']);
+            break;
+          case 403:
+            errorMessage = 'Access forbidden. Contact the administrator.';
+            break;
+          case 404:
+            errorMessage = 'Requested resource not found.';
+            break;
+          case 500:
+            errorMessage = 'Internal Server Error. Try again later.';
+            break;
+          default:
+            errorMessage = `Error ${error.status}: ${error.message}`;
         }
+      }
 
-        // Show a notification to the user
-        this.snackBar.open(errorMessage, 'Close', {
-          duration: 4000,
-          verticalPosition: 'top',
-          panelClass: ['snackbar-error'],
-        });
+      // Show a notification to the user
+      snackBar.open(errorMessage, 'Close', {
+        duration: 4000,
+        verticalPosition: 'top',
+        panelClass: ['snackbar-error'],
+      });
 
-        return throwError(() => new Error(errorMessage));
-      })
-    );
-  }
+      return throwError(() => new Error(errorMessage));
+    })
+  );
 }
